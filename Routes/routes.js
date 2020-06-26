572572fad76f3dd496186806dd5735e778839b4a
windiarties <windiarties@gmail.com>
2020-06-26 21:43:39 +0700

    change api to login logic

--- Routes/routes.js
+++ Routes/routes.js
@@ -1,14 +1,7 @@
-const coverLogic = require('../BisnisLogic/cover')
-const officialLogic = require('../BisnisLogic/official')
+const logic_api = require('../BisnisLogic/api')
 
 
 module.exports = exports = function (server) {
   //login
-  server.get('/api/cover', coverLogic.readcover)
-  server.get('/api/official', officialLogic.readofficial)
-  server.post('/api/cover', coverLogic.cover)
-  server.post('/api/tambah', coverLogic.tambahcover)
-  server.post('/api/artist', coverLogic.tambahartist)
-  server.get('/api/read', coverLogic.readartist)
-  // server.get('api/hasilscrap', coverLogic.hasilscrap)
+  server.get('/api/login', logic_api.login)
 }
