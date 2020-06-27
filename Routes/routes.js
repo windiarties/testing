4ab00b455c7a9bb1affbb9ce324a1563aa1d4cfd
windiarties <windiarties@gmail.com>
2020-06-27 10:43:16 +0700

    add post to /api/login

--- Routes/routes.js
+++ Routes/routes.js
@@ -4,4 +4,5 @@ const logic_api = require('../BisnisLogic/api')
 module.exports = exports = function (server) {
   //login
   server.get('/api/login', logic_api.login)
+  server.post('/api/login', logic_api.login)
 }
