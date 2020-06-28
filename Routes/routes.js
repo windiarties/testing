bcd71c82150704b5247712a5815c9ae5cfb3a120
windiarties <windiarties@gmail.com>
2020-06-29 04:52:22 +0700

    change routes for post data

--- Routes/routes.js
+++ Routes/routes.js
@@ -3,6 +3,6 @@ const logic_api = require('../BisnisLogic/api')
 
 module.exports = exports = function (server) {
   //login
-  server.get('/api/login', logic_api.login)
+  server.get('/api/login', logic_api.read)
   server.post('/api/login', logic_api.login)
 }
