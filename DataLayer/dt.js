cdb2a03930eafa23b18ba420eb331b5dd0139b29
windiarties <windiarties@gmail.com>
2020-06-29 16:40:46 +0700

    console data

--- DataLayer/dt.js
+++ DataLayer/dt.js
@@ -3,10 +3,29 @@ const DatabaseConnection = require('../Config/dbp.config.json')
 var DB = new pg.Pool(DatabaseConnection.config)
 
 const dt = {
-    readUserData: (callback) => { //res=lempar data ke client    
+    readData: (callback) => { //res=lempar data ke client      
         DB.connect(function (err, client, done) {
             var data = ''
             if (err) {
+                data = err;
+            }
+            client.query('SELECT * FROM account', function (err, result) {
+
+                done()
+                if (err) {
+                    data = err;
+                } else {
+                    data = result.rows
+                }
+                callback(data)
+            })
+        })
+    },
+    readUserData: (callback, username) => { //res=lempar data ke client      
+        DB.connect(function (err, client, done) {
+            var data = ''
+            if (err) {
+
                 data = err;
             }
             client.query('SELECT * FROM account where username=($1)', [username], function (err, result) {
@@ -17,14 +36,11 @@ const dt = {
                 } else {
                     data = result.rows
                 }
+                // console.log("ini data di dt : "+ data)
                 callback(data)
-                console.log("ini datanya :" + JSON.stringify(data))
-
             })
-
         })
-
-    }
+    },
 }
 
 module.exports = dt
