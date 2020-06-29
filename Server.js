bb1db347a5db5db1f660b9d09b37ab3b2e8a885c
windiarties <windiarties@gmail.com>
2020-06-29 16:01:45 +0700

    edit server

--- Server.js
+++ Server.js
@@ -1,12 +1,9 @@
 const restify = require('restify')
 const pg = require('pg')
 const DatabaseConnection = require('./Config/dbp.config.json')
-const middleware = require('restify-cors-middleware')
-const cron = require('node-cron')
-const dt = require('./DataLayer/dt')
-var pool = new pg.Pool(DatabaseConnection.config);// konek database
+const middleware = require("restify-cors-middleware")
 
-
+var pool = new pg.Pool(DatabaseConnection.config);
 pool.connect(function (err) {
     if (err) {
         console.log("not able to get connection " + err);
@@ -14,25 +11,23 @@ pool.connect(function (err) {
 
     } else {
         console.log('[DATABASE] connected')
-        const server = restify.createServer() // create server
-        const port = process.env.PORT || 3003 // process.env.Port untuk di hosting
+        const server = restify.createServer()
+        const port = process.env.PORT || 3001
         const cors = middleware({
-            origins: ['*'],
-            allowHeaders: ['Authorization']
+            origins: ['*']
+           // ,
+           // allowHeaders: ['authorization']
         });
+
         server.pre(cors.preflight)
         server.use(cors.actual)
-        server.use(restify.plugins.queryParser());// parsing params link
-        server.use(restify.plugins.bodyParser({ mapParams: false }));// parsing body 
+        server.use(restify.plugins.queryParser());
+        server.use(restify.plugins.bodyParser({ mapParams: false }));
 
         require('./Routes/routes')(server)
 
-        server.listen(port, () => { //memastikan servver aktif 
-            console.log('[SERVER] running at port ' + port) // memastikan udah jalan
+        server.listen(port, () => {
+            console.log('[SERVER] running at port ' + port)
         })
-        cron.schedule('0 00 * * *', () => {
-            console.log('running a task every minute');
-            dt.login('0')
-        });
     }
 })
