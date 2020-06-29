90f11ec021574aabba018feced2bd3a482322df4
windiarties <windiarties@gmail.com>
2020-06-29 15:17:34 +0700

    change routes to user login

--- Routes/routes.js
+++ Routes/routes.js
@@ -1,9 +1,9 @@
-const logic_api = require('../BisnisLogic/api')
+const user_logic = require('../BisnisLogic/api')
 const logic_token = require('../Token/authtoken')
 
 
 module.exports = exports = function (server) {
   //login
 server.get('/api/login', logic_api.read)
-server.post('/api/login', logic_token.checkToken)
+server.post('/api/user', user_logic.login)
 }
