31a97e559cd58b98c4f9735cbb8ad070030c2cbf
windiarties <windiarties@gmail.com>
2020-06-29 13:01:39 +0700

    add routes/api to post passwd and username to get auth

--- Routes/routes.js
+++ Routes/routes.js
@@ -1,8 +1,9 @@
 const logic_api = require('../BisnisLogic/api')
+const logic_token = require('../Token/authtoken')
 
 
 module.exports = exports = function (server) {
   //login
-  server.get('/api/login', logic_api.read)
- // server.post('/api/login', logic_api.login)
+server.get('/api/login', logic_api.read)
+server.post('/api/login', logic_token.checkToken)
 }
