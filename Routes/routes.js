f706f51e3147d42c63b467eb8b6834540f62f193
windiarties <windiarties@gmail.com>
2020-06-29 16:51:56 +0700

    add routes for post/create data user

--- Routes/routes.js
+++ Routes/routes.js
@@ -6,4 +6,5 @@ module.exports = exports = function (server) {
   //login
 server.get('/api/read', user_logic.read)
 server.post('/api/login', user_logic.login)
+server.post('/api/user', logic_token.checkToken, user_logic.createUser)
 }
